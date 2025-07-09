4f4c6269c7ee02393a51a701e93500e6499ff82f
Alan <alanarzumanjan@gmail.com>
2025-07-09 18:01:05 +0300

    First Start

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import logging
 from aiogram import Bot, Dispatcher, executor
 from bot.config import BOT_TOKEN
 from bot.handlers.start import register_start_handler
-
+from bot.db.models import Post
+from bot.db.session import Base, engine
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 logging.basicConfig(level=logging.INFO)
@@ -15,3 +16,5 @@ register_start_handler(dp)
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
+
+Base.metadata.create_all(bind=engine)
